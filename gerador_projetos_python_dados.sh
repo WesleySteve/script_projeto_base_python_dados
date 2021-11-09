8ee78f65f55e63738f875744256426f308779c18
WesleySteve <wesley.silva23@hotmail.com>
2021-11-09 12:08:56 -0300

    função criar iniciada e adicionada no menu de entrada!

--- gerador_projetos_python_dados.sh
+++ gerador_projetos_python_dados.sh
@@ -18,12 +18,33 @@ EOF
 
 # ----------------------- fim help do programa -------------------------- #
 
+# ----------------- inicio criar estrutura de diretórios ------------------ #
+
+_CRIAR() {
+
+    local _nome=''
+
+    read -p "Digite o nome do projeto: " _nome
+
+    if [[ -z $_nome ]] 
+    then
+        echo "NOME DO PROJETO NÃO DEFINIDOR!"
+        exit 1 ;
+
+    
+
+    fi
+
+
+}
+
+# ------------------- fim criar estrutura de diretórios ------------------- #
 
 # ---------------------------- menu entrada ------------------------------- #
 
 case "$1" in
     -h|--help) _AJUDA       ;;
-
+    -c|--criar) _CRIAR      ;;
     *) _AJUDA
 esac
 

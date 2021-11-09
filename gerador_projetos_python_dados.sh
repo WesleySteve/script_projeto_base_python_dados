2d7eee6f325c7357fc0981328a3a80fc7b1c06c6
WesleySteve <wesley.silva23@hotmail.com>
2021-11-09 11:38:45 -0300

    função ajuda do programa

--- gerador_projetos_python_dados.sh
+++ gerador_projetos_python_dados.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
 (($UID == 0)) && { echo "ROOT não!" ; exit 1 ; }
+
+
+# ------------------------- help do programa ------------------------------ #
+
+_AJUDA() {
+
+    cat << EOF
+        OPÇÕES DISPONIVEIS
+            -h ou --help
+            -c ou --criar
+EOF
+}
+
+# ------------------------ fim help do programa --------------------------- #
